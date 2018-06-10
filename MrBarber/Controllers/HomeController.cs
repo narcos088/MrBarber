9f12153c1d395252fd434eb70759d8696bc479a0
Francisco Araújo <Francisco Arajo@DESKTOP-K682GFE>
2018-06-10 01:28:02 +0100

    login feito

--- MrBarber/Controllers/HomeController.cs
+++ MrBarber/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -57,8 +59,75 @@ namespace MrBarber.Controllers
 
             
         }
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string uname,string psw)
+        {
+
+            using (MrBarberDatabaseEntities db = new MrBarberDatabaseEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    var clientes = (from m in db.Clientes
+                                    where m.Email == uname && m.Password == psw
+                                    select m);
+
+                    if(clientes.ToList<Cliente>().Count> 0)
+                    {
+                        return View();
+                    }
+            }
+
+            }
+               
+
+            //string username = null;
+
+            /*
+            SqlConnection sqlConnection1 = new SqlConnection("Data Source = DESKTOP - K682GFE; Initial Catalog = MrBarberDatabase; Integrated Security = True; MultipleActiveResultSets = True; Application Name = EntityFramework");
+
+            using (sqlConnection1) {
+                sqlConnection1.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT Email FROM Cliente where Email=@uname",sqlConnection1);
+
+                
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                         username = reader.GetString(0);
+                    }
+                }
+
+                sqlConnection1.Close();
+
+            }
+            */
+           /* using (MrBarberDatabaseEntities db = new MrBarberDatabaseEntities())
+            {
 
 
+                 //db.Clientes.SqlQuery("SELECT Email FROM Cliente where Email=@uname", new SqlParameter("uname", uname));
+                
 
+                if ("1".Equals(uname)) { return View(); }
+            }*/
+
+
+            //if ("1".Equals(uname)) { return View(); }
+
+            return View("Index");
+        }
     }
 }
